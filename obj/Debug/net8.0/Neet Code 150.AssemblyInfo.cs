8fe92820166879b7cabd9c8d3d221c2ccf0395af
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-05-21 11:52:37 -0300

    Changed Folder Names

--- obj/Debug/net8.0/Neet Code 150.AssemblyInfo.cs
+++ obj/Debug/net8.0/Neet Code 150.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Neet Code 150")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+711ffd196e5afa47b6f9feb67f957d049cdbfe8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd77b8650006a0130aa491c1371a3d0112e105c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Neet Code 150")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Neet Code 150")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
